45c350d626184024e3fe5833aa283bebf381cc13
amilsiddik <amilsiddik@unhas.ac.id>
2021-10-20 20:58:04 +0800

    tes

--- src/com/belajar/OpArray.java
+++ src/com/belajar/OpArray.java
@@ -10,7 +10,27 @@ public class OpArray {
         System.out.println(Arrays.toString(arrayNomor1));
         ngeprint(arrayNomor1);
 
-        //
+        // mengkopi array
+        System.out.println("\nmengkopi array\n");
+        int[] arrayNomor2 = new int[5];
+        ngeprint(arrayNomor1);
+        ngeprint(arrayNomor2);
+        // mengkopi dengan loop
+        for (int i = 0; i < arrayNomor1.length; i++) {
+            arrayNomor2[i] = arrayNomor1[i];
+        }
+        ngeprint(arrayNomor1);
+        ngeprint(arrayNomor2);
+
+        arrayNomor2[2] = 7;
+        ngeprint(arrayNomor1);
+        ngeprint(arrayNomor2);
+
+        // mengkopi dengan copyOf
+        System.out.println("\nmengkopi dengan copyOf\n");
+        int[] arrayNomor3 = Arrays.copyOf(arrayNomor1, 5);
+        ngeprint(arrayNomor3);
+
     }
 
     private static void ngeprint(int[] dataArray) {
--- src/com/belajar/tes.java
+++ src/com/belajar/tes.java
@@ -1,7 +1,5 @@
 package com.belajar;
 
-import java.util.Arrays;
-
 public class tes {
     public static void main(String[] args) {
         System.out.println("Hallo gaes");
