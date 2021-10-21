7ca99585e4f7f426bd8945579618d1dfcc316556
amilsiddik <amilsiddik@unhas.ac.id>
2021-10-21 09:25:56 +0800

    ok

--- src/com/belajar/OpArray.java
+++ src/com/belajar/OpArray.java
@@ -14,6 +14,7 @@ public class OpArray {
         System.out.println("\nmengkopi array\n");
         int[] arrayNomor2 = new int[5];
         ngeprint(arrayNomor1);
+        // System.out.println("array = " + Arrays.toString(arrayNomor1));
         ngeprint(arrayNomor2);
         // mengkopi dengan loop
         for (int i = 0; i < arrayNomor1.length; i++) {
@@ -28,8 +29,45 @@ public class OpArray {
 
         // mengkopi dengan copyOf
         System.out.println("\nmengkopi dengan copyOf\n");
-        int[] arrayNomor3 = Arrays.copyOf(arrayNomor1, 5);
+        int[] arrayNomor3 = Arrays.copyOf(arrayNomor1, 3);
         ngeprint(arrayNomor3);
+
+        // mengkopi dengan copyofrange
+        System.out.println("\nmengkopi dengan copyofrange\n");
+        int[] arraynomor4 = Arrays.copyOfRange(arrayNomor1, 1, 5);
+        ngeprint(arraynomor4);
+
+        // fill
+        System.out.println("\noperasi fill\n");
+        int[] arrayNomor5 = new int[10];
+        Arrays.fill(arrayNomor5, 4);
+        ngeprint(arrayNomor5);
+
+        // komparasi array
+        System.out.println("\noperasi fill\n");
+        int[] arrayNomor6 = { 1, 2, 3, 4, 5 };
+        int[] arrayNomor7 = { 1, 2, 3, 4, 9 };
+
+        System.out.println(Arrays.equals(arrayNomor6, arrayNomor7));
+
+        if (Arrays.equals(arrayNomor6, arrayNomor7)) {
+            System.out.println("array nya sama");
+        } else {
+            System.out.println("array beda");
+        }
+
+        System.out.println(Arrays.compare(arrayNomor6, arrayNomor7));
+
+        // sort array
+
+        int[] arrayNomor8 = { 1, 5, 7, 3, 2, 4 };
+        Arrays.sort(arrayNomor8);
+        ngeprint(arrayNomor8);
+        int[] arrayNomor9 = new int[arrayNomor8.length];
+        for (int i = 0; i < arrayNomor8.length; i++) {
+            arrayNomor9[i] = arrayNomor8[arrayNomor8.length - (i + 1)];
+        }
+        ngeprint(arrayNomor9);
 
     }
 
